969962be444a0aa0543b3cfcc0d0f2e3f4d18ebf
shaikh aamir <101973622+shaikhaamir2508@users.noreply.github.com>
2022-04-10 13:03:44 +0530

    Add files via upload

---
+++ registration.js
@@ -0,0 +1,90 @@
+// console.log("aamir");
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let email = document.getElementById("email");
+let number = document.getElementById("number");
+let validFirstName = false;
+let validLastName = false;
+let validEmail = false;
+let validNumber = false;
+success.classList.remove('show');
+danger.classList.remove('show');
+
+firstName.addEventListener('blur', function () {
+    const regex = /^[a-zA-Z]([a-zA-Z]){3,10}$/;
+    const str = firstName.value;
+    if (regex.test(str)) {
+        firstName.classList.remove('is-invalid');
+        validFirstName = true;
+    }
+    else {
+        firstName.classList.add('is-invalid');
+        validFirstName = false;
+    }
+})
+lastName.addEventListener('blur', function () {
+    const regex = /^[a-zA-Z]([a-zA-Z]){3,10}$/;
+    const str = lastName.value;
+    if (regex.test(str)) {
+        lastName.classList.remove('is-invalid');
+        validLastName = true;
+    }
+    else {
+        lastName.classList.add('is-invalid');
+        validLastName = false;
+    }
+})
+email.addEventListener('blur', function () {
+    const regex = /^([a-zA-Z0-9]+)@([0-9a-zA-Z]+)\.([a-zA-Z])/;
+    const str = email.value;
+    if (regex.test(str)) {
+        email.classList.remove('is-invalid');
+        validEmail = true;
+    }
+    else {
+        email.classList.add('is-invalid');
+        validEmail = false;
+    }
+})
+number.addEventListener('blur', function () {
+    const regex = /^([0-9]){10}$/;
+    const str = number.value;
+    if (regex.test(str)) {
+        number.classList.remove('is-invalid');
+        validNumber = true;
+    }
+    else {
+        number.classList.add('is-invalid');
+        validNumber = false;
+    }
+})
+let submit = document.getElementById('submit');
+submit.addEventListener('click', function (a) {
+    a.preventDefault();
+    
+    if (validFirstName && validLastName && validEmail && validNumber) {
+        let success = document.getElementById('success');
+        let danger = document.getElementById('danger');
+        success.classList.add('show');
+        danger.classList.remove('show');
+
+        let firstName = document.getElementById("firstName").value;
+        let lastName = document.getElementById("lastName").value;
+        let email = document.getElementById("email").value;
+        let number = document.getElementById("number").value;
+      
+        localStorage.setItem("firstName",firstName);
+        localStorage.setItem("lastName",lastName);
+        localStorage.setItem("email",email);
+        localStorage.setItem("number",number);
+
+
+    }
+    else {
+        let danger = document.getElementById('danger');
+        let success = document.getElementById('success');
+        danger.classList.add('show');
+        success.classList.remove('show');
+
+    }
+})
